16b5c95db36504c6ad23270a874231f1159b7663
zyhc <zyhc@zhangyuhaoMacBook-Air.local>
2014-03-13 11:51:13 +0800

    some improve

--- main.cpp
+++ main.cpp
@@ -45,6 +45,25 @@ int main(int argc, const char * argv[])
     sort(hz,hz+i,compare);
     string inpy;
     int t;
+    for (int i=0;i<80;i++)
+    cout<<"-";
+    cout<<endl; 
+    cout<<"Welcome to Typing!"<<endl;
+    cout<<"                                          --created by zhangyuhao"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"                  *******   *     *   *****     ******     "<<endl;
+    cout<<"                     *       *   *    *    *    *          "<<endl;
+    cout<<"                     *        * *     *    *    *          "<<endl;
+    cout<<"                     *         *      *****     ******     "<<endl;
+    cout<<"                     *         *      *         *          "<<endl;
+    cout<<"                     *         *      *         *          "<<endl;
+    cout<<"                     *         *      *         ******     "<<endl;
+    cout<<endl;
+    for (int i=0;i<80;i++)
+    cout<<"-";
+    cout<<endl;
+
     while (1)
     {
         cin>>inpy;
